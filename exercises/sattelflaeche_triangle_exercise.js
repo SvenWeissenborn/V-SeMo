70161491213363b9834e91fc5d0d1f6b8eb15e38
Sven Weissenborn <weisse@uni-hildesheim.de>
2021-09-07 11:44:14 +0200

    Dreieck auf Sattel neues HTML

---
+++ exercises/sattelflaeche_triangle_exercise.js
@@ -0,0 +1,42 @@
+/* -----------Eintragungen--------------*/
+// id: slideID
+// text: "editierbare Textbausteine"
+// sectorsToHide: [SektorID 1, SektorID 2, ...] Array der zu versteckenden Sektoren
+// sectorsToShow: [SektorID 1, SektorID 2, ...] Array der zu zeigenden Sektoren
+// geodesicsToHide: [GeodaeteID 1, GeodaeteID 2, ...] Array der zu versteckenden Geodaeten
+// geodesicsToShow: [GeodaeteID 1, GeodaeteID 2, ...] Array der zu zeigenden Geodaeten
+// marksToHide: [MarkID 1, MarkID 2, ...] Array der versteckenden StartMarks
+// marksToShow: [MarkID 1, MarkID 2, ...] Array der zeigenden StartMarks
+// textsToHide: [TextID 1, TextID 2, ...] Array der versteckenden StartTexts
+// textsToShow: [TextID 1, TextID 2, ...] Array der zeigenden StartTexts
+// startToRemoveAllLines: true; um alle Geodaeten zu loeschen. Beachte, dass die History geleert wird
+// geodesicsToAutoSetAlong: [GeodaeteID 1, GeodaeteID 2, ...], Array der Geodaeten, entlang derer die Sektoren zusammengesetzt werden sollen
+// sectorsToSnapTogether: [[SektorIDToSnap, SektorIDZielsektor], [SektorIDToSnap, SektorIDZielsektor], ...],
+// -> Array der Snappartner. Erster Eintrag gilt dem zu bewegenden Sektor der an den Zielsektor sngesnappt wird
+// slideCondition: [['snappedSectors', [2, 5]]],
+   //textIfSlideConditionIsNotFulfilled: 'Du hast Sektor 5 noch nicht richtig angelegt!'
+
+// imageToAdd: 'add.png' fügt ein Bild ein
+/* -------------------------------------*/
+
+/*  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    Beachte, dass der erste SlideContent
+    alle Sektoren, Geodaeten,Texte und
+    Marks verstecken sollte, die nicht
+    von Anfang an sichtbar sein sollen
+*/
+
+let turnBackwardOff = true
+
+let slideContent = [
+    {
+        id: 0,
+        text_de: 'Vervollständige die beiden Startstriche als Geodäten bis zum Rand des Sektormodells.',
+        text_en: 'Together we want to become familiar with the sector model.',
+    },
+
+
+
+];
+
+
