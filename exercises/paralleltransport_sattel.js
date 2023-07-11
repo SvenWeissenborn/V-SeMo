9fb8689ddf5e7d1e0ffef98e8899e2fa4151d686
TheganMa <stefan-gruene@gmx.de>
2023-07-11 10:51:52 +0200

    html und exercise fuer paralleltransport sattel

---
+++ exercises/paralleltransport_sattel.js
@@ -0,0 +1,45 @@
+/* -----------Eintragungen--------------*/
+// id: slideID
+// text: "editierbare Textbausteine"
+// sectorsToHide: [SektorID 1, SektorID 2, ...] Array der zu versteckenden Sektoren
+// sectorsToShow: [SektorID 1, SektorID 2, ...] Array der zu zeigenden Sektoren
+// geodesicsToHide: [GeodaeteID 1, GeodaeteID 2, ...] Array der zu versteckenden Geodaeten
+// geodesicsToShow: [GeodaeteID 1, GeodaeteID 2, ...] Array der zu zeigenden Geodaeten
+// marksToHide: [MarkID 1, MarkID 2, ...] Array der versteckenden StartMarks
+// marksToShow: [MarkID 1, MarkID 2, ...] Array der zeigenden StartMarks
+// textsToHide: [TextID 1, TextID 2, ...] Array der versteckenden StartTexts
+// textsToShow: [TextID 1, TextID 2, ...] Array der zeigenden StartTexts
+// startToRemoveAllLines: true; um alle Geodaeten zu loeschen. Beachte, dass die History geleert wird
+// geodesicsToAutoSetAlong: [GeodaeteID 1, GeodaeteID 2, ...], Array der Geodaeten, entlang derer die Sektoren zusammengesetzt werden sollen
+// sectorsToSnapTogether: [[SektorIDToSnap, SektorIDZielsektor], [SektorIDToSnap, SektorIDZielsektor], ...],
+// -> Array der Snappartner. Erster Eintrag gilt dem zu bewegenden Sektor der an den Zielsektor sngesnappt wird
+// slideCondition: [['snappedSectors', [2, 5]]],
+   //textIfSlideConditionIsNotFulfilled: 'Du hast Sektor 5 noch nicht richtig angelegt!'
+
+// imageToAdd: 'add.png' fügt ein Bild ein
+/* -------------------------------------*/
+
+/*  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    Beachte, dass der erste SlideContent
+    alle Sektoren, Geodaeten,Texte und
+    Marks verstecken sollte, die nicht
+    von Anfang an sichtbar sein sollen
+*/
+let turnBackwardOff = true;
+
+let slideContent = [
+    {
+        id: 0,
+        text_de: 'Vektortext',
+        text_en: 'Vectortext',
+        sectorsToHide: [2, 5, 6, 7, 8],
+    },
+
+    {
+        id: 1,
+        text_de: 'Vektortext 2',
+        text_en: 'Vectortext 2',
+    },
+];
+
+
